1b68c0a89d1513bedb881ee75d0a856aa097ce8c
Evgeny Ukhanov <mrlsd@ya.ru>
2019-06-24 11:03:51 +0300

    Changed Elf reader - added Zip func to Map data

--- Program.fs
+++ Program.fs
@@ -12,32 +12,22 @@ let printBits (x: int64) =
     let res = System.Convert.ToString(x, 2).PadLeft(64, '0')
     printfn "Bits: %s" res
 
-let rec fetchArr (data : byte array) (acc : uint32) =
-    if data.Length > 1 then
-        [| acc, data.[0] |] |> Array.append (fetchArr data.[1..] (acc - 1u))
-    else
-        [| (acc, data.[0]) |]
+// Help function for fetch Elf data
+let fetchIndexAddr (data : byte array) (index : uint32) =
+    Array.zip [| index .. index + uint32(data.Length) - 1u |] data
 
+let getSectionContent (section : ProgBitsSection<uint32>) =
+    if section.Flags.HasFlag SectionFlags.Executable then
+        fetchIndexAddr (section.GetContents()) section.LoadAddress
+    else
+        [||]
+
+
+/// Read Elf data content to Map data with format: [address, dataByte]
 let readElfFile =
 //    let elf = ELFReader.Load "add32.elf"
     let elf = ELFReader.Load "and32.elf"
-
-    let mutable mem = Map([0u, 0uy])
-    for section in elf.GetSections<ProgBitsSection<uint32>>() do
-        if section.Flags.HasFlag SectionFlags.Executable then
-            let content = section.GetContents()
-            fetchArr content (uint32(content.Length) - 1u + uint32(section.LoadAddress))
-            |> Array.iter (fun (i, v) -> printfn "0x%x | %A" i v )
-//            let mutable ind = la
-//            for cnt in section.GetContents() do
-//                mem <- Map.add ind cnt mem
-//                ind <- ind + 1u
-
-    //Map.iter (fun k v -> printfn "%d %A" k v) mem
-
-//    let elf = ELFReader.Load "rv32mi.elf"
-//        printfn "S: %A | %A" (s.Name) (s.GetContents().Length)
-    Array.concat [| for x in elf.GetSections() -> if x.Name = ".text.init" then x.GetContents() else [||] |]
+    Map.ofArray (Array.concat [| for s in elf.GetSections() -> getSectionContent s |])
 
 let rec runCycle (binData : byte array) (mstate : MachineState) =
     let instr = FetchInstruction binData mstate
@@ -55,8 +45,9 @@ let rec runCycle (binData : byte array) (mstate : MachineState) =
 [<EntryPoint>]
 let main argv =
     let data = readElfFile
+    data |> Map.iter (fun k v -> printfn "0x%x | %A" k v )
 //    let mstate = InitMachineState
 //    do runCycle data mstate
 
-    printfn "Program data length: %A" data.Length
+//    printfn "Program data length: %A" Map
     0 // return an integer exit code
