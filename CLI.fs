1f2e980756f763008e7d5a832a52643abfd6c638
Evgeny Ukhanov <mrlsd@ya.ru>
2019-06-28 00:42:53 +0300

    Fix CLI parser

--- CLI.fs
+++ CLI.fs
@@ -1,5 +1,6 @@
 module ISA.RISCV.CLI
 
+open ISA.RISCV
 open System
 
 let version = "v0.1.0"
@@ -8,20 +9,20 @@ let about = sprintf "RISC-V Simulator for Formal RISC-V ISA implementation\n%s %
 
 type AppConfig = {
         Verbosity: bool option
-        Arch:      string
+        Arch:      string[]
         Files:     string[]
     } with
     static member Default =
         {
             Verbosity = None
-            Arch      = ""
+            Arch      = [||]
             Files     = [||]
         }
 
 type CliResult =
     | Result of AppConfig
     | Error
-    | NotFound
+    | NotFound of AppConfig
 
 type CliOptions = {
         Key:         string option
@@ -63,16 +64,105 @@ let CliUsage (cliArgs : CliOptions []) =
     for arg in cliArgs do
         arg.printHelpMessage
 
+let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) (index : int) =
+    if argv.Length < 1 then
+        (NotFound(cfg), [||])
+    else
+        let arg = argv.[0]
+        let res =
+            if opts.Key.IsSome then
+                if  sprintf "-%s" opts.Key.Value = arg then
+                    if opts.Value.IsSome then
+                        if argv.Length > 1 then
+                            let arg2 = argv.[1]
+                            // Check is value not argument parameter
+                            if "-" = arg2.[..0] then
+                                (Error, 0)
+                            else
+                                let cfg = opts.Handler arg2 cfg
+                                (Result(cfg), 2)
+                        else
+                            (Error, 0)
+                    else
+                        let cfg = opts.Handler arg cfg
+                        printfn "Key: %s | %A" opts.Key.Value cfg
+                        (Result(cfg), 1)
+                else if opts.LongKey.IsSome && sprintf "--%s" opts.LongKey.Value = arg then
+                    if opts.Value.IsSome then
+                        if argv.Length > 1 then
+                            let arg2 = argv.[1]
+                            if "-" = arg2.[..0] then
+                                (Error, 0)
+                            else
+                                let cfg = opts.Handler arg2 cfg
+                                (Result(cfg), 2)
+                        else
+                            (Error, 0)
+                    else
+                        let cfg = opts.Handler arg cfg
+                        printfn "LongKey: %s | %A" opts.LongKey.Value cfg
+                        (Result(cfg), 1)
+                else
+                    (NotFound(cfg), 0)
+
+            else if opts.LongKey.IsSome then
+                if sprintf "--%s" opts.LongKey.Value = arg then
+                    if opts.Value.IsSome then
+                        if argv.Length > 1 then
+                            let arg2 = argv.[1]
+                            if "-" = arg2.[..0] then
+                                (Error, 0)
+                            else
+                                let cfg = opts.Handler arg2 cfg
+                                printfn "LongKey: %s | %A" opts.LongKey.Value cfg
+                                (Result(cfg), 2)
+                        else
+                            (Error, 0)
+                    else
+                        let cfg = opts.Handler arg cfg
+                        printfn "LongKey: %s | %A" opts.LongKey.Value cfg
+                        (Result(cfg), 1)
+                else
+                    (NotFound(cfg), 0)
+
+            else if opts.Value.IsSome then
+                let cfg = opts.Handler arg cfg
+                printfn "Value: %s | %A" opts.Value.Value cfg
+                (Result(cfg), 1)
+            else
+                (NotFound(cfg), 0)
+
+        let (cfgRes, resIndex) = res
+        let endIndex = if index <= index + resIndex - 1 then index + resIndex - 1 else index
+        printfn "INDEX %A" index
+        match cfgRes with
+        | Result(res) when opts.Multiple && argv.Length - resIndex > 0 ->
+                printfn "RES: %A" res
+                let (cfgRes, resIndexes) = fetchArgs argv.[resIndex..] opts res (index + resIndex)
+                let indexes = Array.append resIndexes [| index .. endIndex |]
+                // If NotFound for that branch loop -
+                // redeclare to Result type
+                let resValue = match cfgRes with
+                               | NotFound(x) -> Result(x)
+                               | _ -> cfgRes
+                (resValue, indexes)
+        | NotFound(res) when argv.Length - resIndex > 0 ->
+                fetchArgs argv.[1..] opts res (index + 1)
+        | _ ->
+                (cfgRes, [| index .. endIndex |])
+
 let InitCLI (argv : string[]) =
     let opts = [|
         { CliOptions.Default with
             Key =  Some("A");
             LongKey = Some("arch");
+            Value = Some("ARCH")
+            Multiple = true
             HelpMessage = "RISC-V architecture. Available: rv32i. Default: rv32i"
             Handler =
                 fun arg cfg ->
                 { cfg with
-                    AppConfig.Arch = arg
+                    AppConfig.Arch = Array.append cfg.Arch [| arg |]
                 }
         };
         { CliOptions.Default with
@@ -103,92 +193,9 @@ let InitCLI (argv : string[]) =
         };
     |]
     let appCfg = AppConfig.Default
-    printfn "Args: %A" argv
 
-    for a in argv do
-        for cl in opts do
-            if cl.Key.IsSome && sprintf "-%s" cl.Key.Value = a then
-                let cfg = cl.Handler cl.Key.Value appCfg
-                printfn "Key: %s | %A" cl.Key.Value cfg
-            else if cl.LongKey.IsSome && sprintf "-%s" cl.Key.Value = a then
-                let cfg = cl.Handler cl.LongKey.Value appCfg
-                printfn "Key: %s | %A" cl.LongKey.Value cfg
-
-
+    for opt in opts do
+        printfn "%A\n" opt
+        let res = fetchArgs argv opt appCfg 0
+        printfn "Parse res: %A\n-------------------\n" res
     opts
-
-let rec fetchArgs (argv : string[]) cl cfg =
-    if argv.Length < 1 then
-        None
-    else
-        let arg = argv.[0]
-        let res =
-            if cl.Key.IsSome then
-                if  sprintf "-%s" cl.Key.Value = arg then
-                    if cl.Value.IsSome then
-                        if argv.Length < 2 then
-                            let arg2 = argv.[1]
-                            // Check is value not argument parameter
-                            if "-" = arg2.[..0] then
-                                (Error, 0)
-                            else
-                                let cfg = cl.Handler arg2 cfg
-                                (Result(cfg), 2)
-                        else
-                            (Error, 0)
-                    else
-                        let cfg = cl.Handler arg cfg
-                        printfn "Key: %s | %A" cl.Key.Value cfg
-                        (Result(cfg), 1)
-                else if cl.LongKey.IsSome && sprintf "-%s" cl.Key.Value = arg then
-                    if cl.Value.IsSome then
-                        if argv.Length < 2 then
-                            let arg2 = argv.[1]
-                            if "-" = arg2.[..0] then
-                                (Error, 0)
-                            else
-                                let cfg = cl.Handler arg2 cfg
-                                printfn "LongKey: %s | %A" cl.LongKey.Value cfg
-                                (Result(cfg), 2)
-                        else
-                            (Error, 0)
-                    else
-                        let cfg = cl.Handler arg cfg
-                        printfn "LongKey: %s | %A" cl.LongKey.Value cfg
-                        (Result(cfg), 1)
-                else
-                    (NotFound, 0)
-
-            else if cl.LongKey.IsSome then
-                if sprintf "-%s" cl.LongKey.Value = arg then
-                    if cl.Value.IsSome then
-                        if argv.Length < 2 then
-                            let arg2 = argv.[1]
-                            if "-" = arg2.[..0] then
-                                (Error, 0)
-                            else
-                                let cfg = cl.Handler arg2 cfg
-                                printfn "LongKey: %s | %A" cl.LongKey.Value cfg
-                                (Result(cfg), 2)
-                        else
-                            (Error, 0)
-                    else
-                        let cfg = cl.Handler arg cfg
-                        printfn "LongKey: %s | %A" cl.LongKey.Value cfg
-                        (Result(cfg), 1)
-                else
-                    (NotFound, 0)
-
-            else if cl.Value.IsSome then
-                let cfg = cl.Handler arg cfg
-                printfn "Value: %s | %A" cl.Value.Value cfg
-                (Result(cfg), 1)
-            else
-                (NotFound, 0)
-
-        let (cfgRes, resIndex) = res
-        match cfgRes with
-        | Result(res) when cl.Multiple && argv.Length - resIndex > 0 -> fetchArgs argv.[resIndex..] cl res
-        | NotFound when argv.Length - resIndex > 0 -> fetchArgs argv.[resIndex..] cl cfg
-        | _ -> Some(cfg)
-
