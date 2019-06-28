d8b5c437b9de19bf1c12ccde20e8f2a4cbf8178a
Evgeny Ukhanov <mrlsd@ya.ru>
2019-06-29 00:12:53 +0300

    Changed CLI logic

--- CLI.fs
+++ CLI.fs
@@ -1,6 +1,6 @@
 module ISA.RISCV.CLI
+#nowarn "40"
 
-open ISA.RISCV
 open System
 
 let version = "v0.1.0"
@@ -9,53 +9,61 @@ let about = sprintf "RISC-V Simulator for Formal RISC-V ISA implementation\n%s %
 
 type AppConfig = {
         Verbosity: bool option
-        Arch:      string[]
-        Files:     string[]
+        Arch:      string option
+        Files:     string[] option
     } with
     static member Default =
         {
             Verbosity = None
-            Arch      = [||]
-            Files     = [||]
+            Arch      = None
+            Files     = None
         }
+    member x.CheckRequired =
+        if x.Files.IsNone || x.Arch.IsNone then
+            false
+        else
+            true
 
 type CliResult =
     | Result of AppConfig
     | Error
     | NotFound of AppConfig
 
+type CliStatus =
+    | Success of AppConfig
+    | Stopped
+    | Failed
+
 type CliOptions = {
-        Key:         string option
-        LongKey:     string option
-        Value:       string option
-        Multiple:    bool
-        HelpMessage: string
-        Required:    bool
-        IsFound:     bool
-        Handler:     (string -> AppConfig -> AppConfig)
+        Key:           string option
+        LongKey:       string option
+        Value:         string option
+        Multiple:      bool
+        HelpMessage:   string
+        StopExecution: bool
+        Handler:       (string -> AppConfig -> AppConfig)
     } with
     static member Default =
         {
-            Key         = None
-            LongKey     = None
-            Value       = None
-            Multiple    = false
-            HelpMessage = ""
-            Required    = false
-            IsFound     = false
-            Handler     = fun _ cfg -> cfg
+            Key           = None
+            LongKey       = None
+            Value         = None
+            Multiple      = false
+            HelpMessage   = ""
+            StopExecution = false
+            Handler       = fun _ cfg -> cfg
         }
     member x.printHelpMessage =
         let msg = if x.Key.IsSome && x.LongKey.IsSome then
-                    sprintf "-%s, --%s" x.Key.Value x.LongKey.Value
+                      sprintf "-%s, --%s" x.Key.Value x.LongKey.Value
                   else if x.Key.IsSome then
-                    sprintf "-%s\t" x.Key.Value
+                      sprintf "-%s\t" x.Key.Value
                   else if x.LongKey.IsSome then
-                    sprintf "--%s\t" x.LongKey.Value
+                      sprintf "--%s\t" x.LongKey.Value
                   else if x.Value.IsSome then
-                    sprintf "<%s>\t" x.Value.Value
+                      sprintf "<%s>\t" x.Value.Value
                   else
-                    ""
+                      ""
         printfn "%s" (String.Format("{0,-5}{1,-20} {2}", "", msg, x.HelpMessage))
 
 let CliUsage (cliArgs : CliOptions []) =
@@ -64,12 +72,12 @@ let CliUsage (cliArgs : CliOptions []) =
     for arg in cliArgs do
         arg.printHelpMessage
 
-let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) (index : int) =
+let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) =
     if argv.Length < 1 then
-        (NotFound(cfg), [||])
+        NotFound(cfg)
     else
         let arg = argv.[0]
-        let res =
+        let (cfgRes, resIndex) =
             if opts.Key.IsSome then
                 if  sprintf "-%s" opts.Key.Value = arg then
                     if opts.Value.IsSome then
@@ -85,7 +93,6 @@ let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) (index
                             (Error, 0)
                     else
                         let cfg = opts.Handler arg cfg
-                        printfn "Key: %s | %A" opts.Key.Value cfg
                         (Result(cfg), 1)
                 else if opts.LongKey.IsSome && sprintf "--%s" opts.LongKey.Value = arg then
                     if opts.Value.IsSome then
@@ -100,7 +107,6 @@ let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) (index
                             (Error, 0)
                     else
                         let cfg = opts.Handler arg cfg
-                        printfn "LongKey: %s | %A" opts.LongKey.Value cfg
                         (Result(cfg), 1)
                 else
                     (NotFound(cfg), 0)
@@ -114,56 +120,66 @@ let rec fetchArgs (argv : string[]) (opts : CliOptions) (cfg : AppConfig) (index
                                 (Error, 0)
                             else
                                 let cfg = opts.Handler arg2 cfg
-                                printfn "LongKey: %s | %A" opts.LongKey.Value cfg
-                                (Result(cfg), 2)
+                                (Result(cfg), 0)
                         else
                             (Error, 0)
                     else
                         let cfg = opts.Handler arg cfg
-                        printfn "LongKey: %s | %A" opts.LongKey.Value cfg
                         (Result(cfg), 1)
                 else
                     (NotFound(cfg), 0)
 
             else if opts.Value.IsSome then
                 let cfg = opts.Handler arg cfg
-                printfn "Value: %s | %A" opts.Value.Value cfg
                 (Result(cfg), 1)
             else
                 (NotFound(cfg), 0)
 
-        let (cfgRes, resIndex) = res
-        let endIndex = if index <= index + resIndex - 1 then index + resIndex - 1 else index
-        printfn "INDEX %A" index
         match cfgRes with
         | Result(res) when opts.Multiple && argv.Length - resIndex > 0 ->
-                printfn "RES: %A" res
-                let (cfgRes, resIndexes) = fetchArgs argv.[resIndex..] opts res (index + resIndex)
-                let indexes = Array.append resIndexes [| index .. endIndex |]
-                // If NotFound for that branch loop -
-                // redeclare to Result type
-                let resValue = match cfgRes with
-                               | NotFound(x) -> Result(x)
-                               | _ -> cfgRes
-                (resValue, indexes)
-        | NotFound(res) when argv.Length - resIndex > 0 ->
-                fetchArgs argv.[1..] opts res (index + 1)
-        | _ ->
-                (cfgRes, [| index .. endIndex |])
+            let cfgRes = fetchArgs argv.[resIndex..] opts res
+            // If NotFound for that branch loop -
+            // redeclare to Result type
+            let resValue = match cfgRes with
+                           | NotFound(x) -> Result(x)
+                           | _ -> cfgRes
+            resValue
+        | NotFound(res) when argv.Length > 0 ->
+            fetchArgs argv.[1..] opts res
+        | _ -> cfgRes
 
-let InitCLI (argv : string[]) =
-    let opts = [|
+// Parse CLI with specific params
+let rec parseCli (argv : string[]) (opts : CliOptions[]) (cfg : AppConfig) =
+    if opts.Length < 1 then
+        Success(cfg)
+    else
+        let opt = opts.[0]
+        let opts = if opts.Length > 1 then opts.[1..] else [||]
+        let resCfg = fetchArgs argv opt cfg
+        match resCfg with
+        | Error ->
+            Failed
+        | NotFound(cfg) ->
+            parseCli argv opts cfg
+        | Result(cfg) ->
+            if opt.StopExecution then
+                Stopped
+            else
+                parseCli argv opts cfg
+
+// Init CLI options and arguments
+let rec InitCLI =
+    [|
         { CliOptions.Default with
             Key =  Some("A");
             LongKey = Some("arch");
             Value = Some("ARCH")
-            Multiple = true
             HelpMessage = "RISC-V architecture. Available: rv32i. Default: rv32i"
             Handler =
                 fun arg cfg ->
-                { cfg with
-                    AppConfig.Arch = Array.append cfg.Arch [| arg |]
-                }
+                    { cfg with
+                        AppConfig.Arch = Some(arg)
+                    }
         };
         { CliOptions.Default with
             Key =  Some("v");
@@ -178,24 +194,32 @@ let InitCLI (argv : string[]) =
             Key =  Some("h");
             LongKey = Some("help");
             HelpMessage = "Print help message"
+            StopExecution = true
             Handler =
                 fun arg cfg ->
+                    CliUsage InitCLI
                     cfg
         };
         { CliOptions.Default with
             Key =  Some("V");
             LongKey = Some("version");
             HelpMessage = "Application version"
+            StopExecution = true
             Handler =
                 fun _ cfg ->
                     printfn "%s" about
                     cfg
         };
-    |]
-    let appCfg = AppConfig.Default
+        { CliOptions.Default with
+            HelpMessage = "Files to executions"
+            Value = Some("FILE")
+            Multiple = true
+            Handler =
+                fun arg cfg ->
+                    let res = if cfg.Files.IsSome then cfg.Files.Value else [||]
+                    { cfg with
+                        AppConfig.Files = Some(Array.append res [| arg |])
+                    }
 
-    for opt in opts do
-        printfn "%A\n" opt
-        let res = fetchArgs argv opt appCfg 0
-        printfn "Parse res: %A\n-------------------\n" res
-    opts
+        };
+    |]
--- Program.fs
+++ Program.fs
@@ -4,8 +4,13 @@ open ISA.RISCV
 
 [<EntryPoint>]
 let main argv =
-    let str = "test"
-    printfn "%s" str.[..0]
-
-    let _ = CLI.InitCLI argv
+    let cfg = CLI.parseCli argv CLI.InitCLI CLI.AppConfig.Default
+    match cfg with
+    | CLI.Failed -> failwith "Failed parse CLI params. Print --help"
+    | CLI.Stopped -> ()
+    | CLI.Success(x) ->
+        if not x.CheckRequired then
+            printfn "Wrong parameters put --help to get more information"
+        else
+            printfn "CFG: %A" cfg
     0 // return an integer exit code
