e33cb2acf514e40199c536120c4029b4ed5c24c0
Evgeny Ukhanov <mrlsd@ya.ru>
2019-09-01 23:41:56 +0300

    Added common rv32i tests (without memory) and Assembler files for tests

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -47,7 +47,7 @@ let ``SLTI: x3 = x2 < 5`` (x2, x3) =
 [<InlineData(-4, 0)>]
 [<InlineData(-5, 0)>]
 [<InlineData(-10, 0)>]
-let ``SLTIU: x3 = unsign x2 < 5`` (x2, x3) =
+let ``SLTIU: x3 = unsign x2 < unsign 5`` (x2, x3) =
     ALUimmediate 0x00513193L x2 x3
 
 [<Theory>]
@@ -58,3 +58,80 @@ let ``SLTIU: x3 = unsign x2 < 5`` (x2, x3) =
 [<InlineData(0b1011, 0b1110)>]
 let ``XORI: x3 = x2 ^ 5 (b101)`` (x2, x3) =
     ALUimmediate 0x00514193L x2 x3
+
+//-------------
+// TODO: set tests
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``ORI: x3 = x2 | 5 (b101)`` (x2, x3) =
+    ALUimmediate 0x00516193L x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``ANDI: x3 = x2 & 5 (b101)`` (x2, x3) =
+    ALUimmediate 0x00517193L x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SLLI: x3 = x2 >> 5 (b101)`` (x2, x3) =
+    ALUimmediate 0x00511193L x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SRLI: x3 = x2 << 5 (b101)`` (x2, x3) =
+    ALUimmediate 0x00515193L x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SRAI: x3 = x2 << 5 (b101)`` (x2, x3) =
+    ALUimmediate 0x40515193L x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``ADD: x3 = x2 + x1`` (x1, x2, x3) =
+    ALU 0x001101b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SUB: x3 = x2 - x1`` (x1, x2, x3) =
+    ALU 0x401101b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SLL: x3 = x2 << x1`` (x1, x2, x3) =
+    ALU 0x001111b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SLT: x3 = x2 < x1`` (x1, x2, x3) =
+    ALU 0x001121b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SLTU: x3 = unsign x2 << unsign x1`` (x1, x2, x3) =
+    ALU 0x001131b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``XOR: x3 = x2 ^ x1`` (x1, x2, x3) =
+    ALU 0x001141b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SRL: x3 = x2 >> x1`` (x1, x2, x3) =
+    ALU 0x001151b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``SRA: x3 = x2 >> x1`` (x1, x2, x3) =
+    ALU 0x401151b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``OR: x3 = x2 | x1`` (x1, x2, x3) =
+    ALU 0x001161b3L x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0)>]
+let ``AND: x3 = x2 & x1`` (x1, x2, x3) =
+    ALU 0x001171b3L x1 x2 x3
