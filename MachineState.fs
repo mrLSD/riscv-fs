f3d482d18c2aad6843fd38ec52cb512803782ff9
Evgeny Ukhanov <mrlsd@ya.ru>
2019-10-11 04:03:53 +0300

    Test ADDI - added <Theory> common test

--- MachineState.fs
+++ MachineState.fs
@@ -31,20 +31,20 @@ type MachineState = {
         { x with Registers = registers }
 
     member x.setPC (pc : MachineInt) : MachineState =
-        { x with PC = x.alignByArch pc }
+        { x with PC = x.alignByArchUnsign pc }
 
     member x.incPC : MachineState =
-        { x with PC = x.alignByArch (x.PC + 4L) }
+        x.setPC (x.PC + 4L)
 
     member x.getMemory(addr : int64) =
-        let addr = x.alignByArch addr
+        let addr = x.alignByArchUnsign addr
         if Map.containsKey addr x.Memory then
             Some(x.Memory.[addr])
         else
             None
 
     member x.setMemoryByte (addr : int64) (value : byte) : MachineState =
-        let addr = x.alignByArch addr
+        let addr = x.alignByArchUnsign addr
         let mem = Map.add addr (byte value) x.Memory
         { x with Memory = mem }
 
@@ -54,8 +54,15 @@ type MachineState = {
         // if x32 Arch - align it to x32
         // and then convert again to int64
         match x.Arch.archBits with
-        | Architecture.RV32 -> int64( uint32(value) )
+        | Architecture.RV32 -> int64( int32(value) )
         | _ -> value
+
+    member x.alignByArchUnsign (value : int64) =
+        // if x32 Arch - align it to x32
+        // and then convert again to int64
+        match x.Arch.archBits with
+        | Architecture.RV32 -> int64( uint32(value) )
+        | _ -> int64( uint64(value) )
 
 let InitMachineState mem arch verbosity : MachineState =
     {
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -4,31 +4,26 @@ open Microsoft.FSharp.Collections
 open Xunit
 
 open ISA.RISCV
-open ISA.RISCV
-open ISA.RISCV.Utils.Bits
 open ISA.RISCV.Arch
 open ISA.RISCV.Decode
 
-[<Fact>]
-let ``ADDI to zero`` () =
+let ALUimmediate instr x2 x3 =
     // Init MachineState
     let addr = 0x80000000L
     let mstate = MachineState.InitMachineState Map.empty RV32i true
-    let mstate = mstate.setPC 0x80000000L
+    let mstate = mstate.setPC addr
+    let mstate = mstate.setRegister 2 x2
 
-    // x2 = x0 + 3
-    let instr = 0x00300113L
     let decodedInstr = I.DecodeI instr
     Assert.NotEqual(decodedInstr, I.None)
     let mstate = ExecuteI.ExecuteI decodedInstr mstate
+    Assert.Equal(x2, mstate.getRegister 2)
+    Assert.Equal(x3, mstate.getRegister 3)
+    Assert.Equal(addr + 4L, mstate.PC)
 
-    // x3 = x2 + 5
-    let instr = 0x00510193L
-    let decodedInstr = I.DecodeI instr
-    Assert.NotEqual(decodedInstr, I.None)
-    let mstate = ExecuteI.ExecuteI decodedInstr mstate
-
-    Assert.Equal(mstate.getRegister 2, 3L)
-    Assert.Equal(mstate.getRegister 3, 8L)
-
-    Assert.Equal(addr + 8L, mstate.PC)
+[<Theory>]
+[<InlineData(5, 10)>]
+[<InlineData(-5, 0)>]
+[<InlineData(-10, -5)>]
+let ``ADDI: x3 = x2 + 5`` (x2, x3) =
+    ALUimmediate 0x00510193L x2 x3
