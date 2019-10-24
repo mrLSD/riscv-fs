b94f1ea9aee75c4747cdeffe836b014a463b9bbd
Evgeny Ukhanov <mrlsd@ya.ru>
2019-10-24 17:37:22 +0300

    Adde tests: LUI, AUIPC

--- Tests/rv32i/ui.fs
+++ Tests/rv32i/ui.fs
@@ -8,7 +8,7 @@ open ISA.RISCV.Decode
 
 //===============================================
 // Upper immediate tests
-let Ui instr imm =
+let Ui instr x3 =
     // Init MachineState
     let addr = 0x80000000L
     let mstate = MachineState.InitMachineState Map.empty RV32i true
@@ -17,4 +17,18 @@ let Ui instr imm =
     let decodedInstr = I.DecodeI instr
     Assert.NotEqual(decodedInstr, I.None)
     let mstate = ExecuteI.ExecuteI decodedInstr mstate
-    mstate.incPC
+    Assert.Equal(x3, mstate.getRegister 3)
+
+[<Theory>]
+[<InlineData(0x0000a1b7, 0xA000L)>]
+[<InlineData(0x000001b7, 0x0L)>]
+[<InlineData(0xfffff1b7, 0xfffffffffffff000L)>]
+let ``LUI: x3, imm20`` (instr, x3) =
+    Ui instr x3
+
+[<Theory>]
+[<InlineData(0x0000a197, 0xffffffff8000a000L)>]
+[<InlineData(0x00000197, 0xffffffff80000000L)>]
+[<InlineData(0xfffff197, 0x7ffff000L)>]
+let ``AUIPC: x3, imm20`` (instr, x3) =
+    Ui instr x3
