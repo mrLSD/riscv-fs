10477545fc22627ccfd05970268d772e2502cc57
Evgeny Ukhanov <mrlsd@ya.ru>
2019-11-07 00:58:34 +0200

    Fix rv64i ALU instructions. Added completed W-tests for rv64i ALU

--- Tests/rv64i/alu.fs
+++ Tests/rv64i/alu.fs
@@ -114,3 +114,56 @@ let ``OR: x3 = x2 | x1`` (x1, x2, x3) =
 [<InlineData(0b101, 0b1011, 0b0001)>]
 let ``AND: x3 = x2 & x1`` (x1, x2, x3) =
     ALU 0x001171b3 x1 x2 x3
+
+[<Theory>]
+[<InlineData(10, 20, 30)>]
+[<InlineData(0, 20, 20)>]
+[<InlineData(-10, 20, 10)>]
+[<InlineData(-40, 20, -20)>]
+[<InlineData(0xFFFFFFFF, 10, 9)>]
+[<InlineData(0xFACFFFFFFFFL, 10, 9)>]
+[<InlineData(0xFACFFFFFFFFL, 10, 9)>]
+[<InlineData(0xFCD3FFA1FFFFFFFEL, 10, 8)>]
+let ``ADDW: x3 = x2 + x1`` (x1, x2, x3) =
+    ALU 0x001101bb x1 x2 x3
+
+[<Theory>]
+[<InlineData(20, 10, -10)>]
+[<InlineData(10, 20, 10)>]
+[<InlineData(0, 20, 20)>]
+[<InlineData(10, 0, -10)>]
+[<InlineData(-10, -20, -10)>]
+[<InlineData(10, -20, -30)>]
+[<InlineData(10, 0xFFFFFFFF, -11)>]
+[<InlineData(0xFFFFFFFF, 10, 11)>]
+[<InlineData(0xFACFFFFFFFFL, 10, 11)>]
+[<InlineData(0xFCD3FFA1FFFFFFFEL, 10, 12)>]
+let ``SUBW: x3 = x2 - x1`` (x1, x2, x3) =
+    ALU 0x401101bb x1 x2 x3
+
+[<Theory>]
+[<InlineData(5, 0b101101, 0b10110100000)>]
+[<InlineData(0, 0b101101, 0b101101)>]
+[<InlineData(5, 0b11001101, 0b1100110100000)>]
+[<InlineData(5, 0b11001110, 0b1100111000000)>]
+[<InlineData(5, 0xFFFFFFFF8AF300C7L, 0x5e6018e0L)>]
+[<InlineData(5, 0xFFFFFFFF8FF3F0C7L, 0xfffffffffe7e18e0L)>]
+let ``SLLW: x3 = x2 << x1`` (x1, x2, x3) =
+    ALU 0x001111bb x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0b1011001101, 0b0000010110)>]
+[<InlineData(0b101, 0b11001100101, 0b00000110011)>]
+[<InlineData(0b101, 0b11110000111100000000000000001111, 0x7878000L)>]
+[<InlineData(0b101, 0x8FAFCF1F8AF300C7L, 0x4579806L)>]
+[<InlineData(0b101, 0xFFDFCF1F8AF300C7L, 0x4579806L)>]
+let ``SRLW: x3 = x2 >> x1`` (x1, x2, x3) =
+    ALU 0x001151bb x1 x2 x3
+
+[<Theory>]
+[<InlineData(0b101, 0b11110000111100000000000000001111, 0b11111111100001111000000000000000)>]
+[<InlineData(0b101, 0b1011001101, 0b0000010110)>]
+[<InlineData(0b101, 0xFFFFFFFF6AF300C7L, 0x3579806L)>]
+[<InlineData(0b101, 0xFFFFFFFF8AF300C7L, 0xfffffffffc579806L)>]
+let ``SRAW: x3 = x2 >> x1`` (x1, x2, x3) =
+    ALU 0x401151bb x1 x2 x3
