d1bcffaf9a5c9375f28cff9f1cb20cd0c564745e
Evgeny Ukhanov <mrlsd@ya.ru>
2019-12-15 16:37:19 +0200

    Extend AMO Decoder

--- DecodeA.fs
+++ DecodeA.fs
@@ -11,4 +11,44 @@ type InstructionA =
     | LR_W   of  {| rd: Register; rs1: Register; aq: InstrField; rl: InstrField |}
     | SC_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
 
+    | AMOSWAP_W  of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOADD_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOXOR_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOAND_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOOR_W    of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOMIN_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOMAX_W   of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOMINU_W  of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    | AMOMAXU_W  of  {| rd: Register; rs1: Register; rs2: Register; aq: InstrField; rl: InstrField |}
+    
     | None // Instruction not found
+
+/// Decode 'A' instructions
+let Decode (instr: InstrField) : InstructionA =
+    let opcode = instr.bitSlice 6 0
+    // Register number can be: 0-32
+    let rd     = int32(instr.bitSlice 11  7)
+    let rs1    = int32(instr.bitSlice 19 15)
+    let rs2    = int32(instr.bitSlice 24 20)
+    
+    let funct3 = instr.bitSlice 14 12
+    let funct7 = instr.bitSlice 31 25
+    
+    match (opcode) with
+    | 0b0101111 when funct3 = 0b010 -> None
+    
+    | _ -> None
+
+let verbosityMessage (instr : InstrField) (decodedInstr : InstructionA) (mstate : MachineState) =
+    let typeName = decodedInstr.GetType().Name
+    let instrMsg =
+        match (decodedInstr) with
+        | LR_W x -> sprintf "x%d, x%d" x.rd x.rs1    
+        | SC_W x | AMOSWAP_W x | AMOADD_W x | AMOXOR_W x
+        | AMOAND_W x | AMOOR_W x | AMOMIN_W x
+        | AMOMAX_W x | AMOMINU_W x | AMOMAXU_W x-> sprintf "x%d, x%d, x%d" x.rd x.rs1 x.rs2
+        | _ -> "Undef"
+    let pc = sprintf "%08x:" mstate.PC
+    let instr = sprintf "%08x" instr
+    let instrMsg = String.Format("{0,-7}{1}", typeName, instrMsg)
+    printfn "%s" (String.Format("{0,-12}{1,-12}{2}", pc, instr, instrMsg))
