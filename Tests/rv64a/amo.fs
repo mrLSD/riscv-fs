5d3c78c42d8d1da34d833002dc9d39964337216c
Evgeny Ukhanov <mrlsd@ya.ru>
2020-05-29 12:23:05 +0300

    AMO: added AMO.XOR tests

--- Tests/rv64a/amo.fs
+++ Tests/rv64a/amo.fs
@@ -88,3 +88,22 @@ let ``AMO.OR`` (a4, a5, a6, a7) =
             0x0006a883
         |]
     ALU instrSet a4 a5 a6 a7
+
+[<Theory>]
+[<InlineData(0xffffffff80000000L, 0x7ffff800L, 0x7ffff800L, 0xffffffffbffff801L)>]
+let ``AMO.XOR`` (a4, a5, a6, a7) =
+    let instrSet = [|
+            0x80000537
+            0x80000593
+            0x00001697
+            0xff868693
+            0x00a6a023
+            
+            0x20b6a72f
+            0x0006a783
+            0xc0000637
+            0x00160613
+            0x20c6a82f
+            0x0006a883
+        |]
+    ALU instrSet a4 a5 a6 a7
