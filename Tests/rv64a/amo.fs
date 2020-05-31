28fd1e33576c9c0046e8aee7f78857163836ef09
Evgeny Ukhanov <mrlsd@ya.ru>
2020-05-31 12:01:38 +0300

    AMO: added AMO.MAX tests

--- Tests/rv64a/amo.fs
+++ Tests/rv64a/amo.fs
@@ -125,3 +125,22 @@ let ``AMO.SWAP`` (a4, a5, a6, a7) =
             0x0006a883
         |]
     ALU instrSet a4 a5 a6 a7
+
+[<Theory>]
+[<InlineData(0xffffffff80000000L, 0xfffffffffffff800L, 0L, 1L)>]
+let ``AMO.MAX`` (a4, a5, a6, a7) =
+    let instrSet = [|
+            0x80000537
+            0x80000593
+            0x00001697
+            0xff868693
+            0x00a6a023
+            
+            0xa0b6a72f
+            0x0006a783
+            0x00100613
+            0x0006a023
+            0xa0c6a82f
+            0x0006a883
+        |]
+    ALU instrSet a4 a5 a6 a7
